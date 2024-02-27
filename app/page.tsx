dc29a0da96035ec93e63c3b6c9216b6fa3b5ad19
samyak swaroop mishra <samyak2k16@gmail.com>
2024-02-27 19:44:43 +0530

    home2/3

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="containerMod">
-      <div className="containerMod mt-9">
-        <h1 className="text-teal-500">hellow</h1>
+      <div className="containerMod p-4 mt-9">
+      
         <img className="w-[100%] maxh-[50vh]" src="/homebanerimg.png" />
         <h1 className=" mt-5 text-center text-3xl font-medium tracking-wider"> Welcome to </h1>
         <h1 className=" mb-7 text-center text-red-800 font-semibold text-3xl tracking-wider"> RAJSURI PRINT & PACK </h1>
@@ -18,7 +18,7 @@ export default function Home() {
           skilled team, we ensure that your printing needs are met with precision professionalism. Whether it's for
           personal or business use, trust Rajsuri Print and pack to bring your ideals to life with clarity and fineness.
         </div>
-        <div className="mt-5 grid grid-cols-2 gap-2 rounded-md w-[100%]">
+        <div className="mt-5 grid grid-cols-2 gap-2 rounded-md w-[100%] lg:grid-cols-3">
 
           <div className="p-2 flex flex-col justify-start items-center">
             <div className="bg-rose-500 rounded-md">
@@ -47,14 +47,14 @@ export default function Home() {
           </div>
 
           <div className=" p-2 flex flex-col justify-start items-center">
-            <div className="bg-rose-500 rounded-md">
-              <img src="/coin-in-hand.png" className="object-cover h-36 w-36" alt="" />
+            <div className="p-2 bg-rose-500 rounded-md">
+              <img src="/Bestseller.svg" className="object-cover h-36 w-36" alt="" />
             </div>
             <div className="font-normal text-center mt-2">Best Quality Printing</div>
           </div>
           <div className=" p-2 flex flex-col justify-start items-center">
             <div className="bg-rose-500 rounded-md">
-              <img src="/coin-in-hand.png" className="object-cover h-36 w-36" alt="" />
+              <img src="/Popularman.svg" className="object-cover p-4 h-36 w-36" alt="" />
             </div>
             <div className="font-normal text-center mt-2">Highly Experienced Staff</div>
           </div>
@@ -71,13 +71,14 @@ export default function Home() {
       {/* client review pink section */}
 
 
-      <div className="bg-rose-500 p-2 w-[100%] mt-5 flex flex-col justify-start items-center">
+      <div className="bg-rose-500  w-[100%] mt-5 flex flex-col justify-start items-center">
         <h1 className="mt-10 text-3xl text-slate-50 font-semibold">What our clients are saying?</h1>
-        <h3 className="mt-10 text-lg text-center text-slate-50 font-normal">
+        <img src="/teamwork.png" className="object-cover h-64 w-72" alt="" />
+        <h3 className="mt-1 text-lg text-center text-slate-50 font-normal">
           “Rajsuri Print has been our go-to for all printing needs at Choudhary Bhupendra Hospital. From hospital files to CT MRI
           covers and marketing materials, their quality and reliability have been outstanding. Highly Recommended!
         </h3>
-        <h2 className="mt-10 text-lg text-center text-slate-50 font-normal">
+        <h2 className="mt-7 text-lg text-center text-slate-50 font-normal">
           -Dhukaram Choudhary
           (Managing Director of CBH Hospital, Bhinmal)
         </h2>
