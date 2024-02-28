1355504bc63838b6a01d38fb0e7c0876e59b2355
siddhant ota <siddhantota@protonmail.com>
2024-02-28 11:42:05 +0530

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/custom/Nav";
+import Footer from "@/components/custom/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Nav />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/custom/Nav.tsx
+++ components/custom/Nav.tsx
@@ -2,30 +2,53 @@ import React from 'react'
 import {
     Dialog,
     DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+
 import {
     Accordion,
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
+
 import Link from 'next/link'
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
 
 
 const Nav = () => {
     return (
         <div className='bg-[#002246]'>
-            <div className='flex justify-between bg-[#002246] p-3 z-50 containerMod'>
+            <div className='flex px-2 py-4 justify-between bg-[#002246] p-3 z-50 containerMod'>
                 <img src="/logo.png" alt="" />
-                <div>
-                    <ul>
+                <div className='hidden md:block'>
+                    <ul className='flex items-center gap-3'>
                         <li className='flex items-center text-white gap-3'>
                             <img src="/Home.png" className='size-7' alt="" /> <p>Home</p>
-                            <img src="/products.png" className='size-7' alt="" /> <p>Our Products</p>
+                        </li>
+                        <li className='flex items-center text-white gap-3'>
+                            <img src="/products.png" className='size-7' alt="" />
+                            <Popover>
+                                <PopoverTrigger>
+                                    <p>Our Products</p>
+                                </PopoverTrigger>
+                                <PopoverContent className='flex flex-col'>
+                                    <Link className='text-xl' href={'/'}>- Hospital files</Link>
+                                    <Link className='text-xl' href='/'>- Aamantran Patrikas</Link>
+                                    <Link className='text-xl' href={'/'}>- X-ray, MRI Covers</Link>
+                                    <Link className='text-xl' href={'/'}>- Printed Shopping Bags</Link>
+                                    <Link className='text-xl' href={'/'}>- Packing Box Printing</Link>
+                                </PopoverContent>
+                            </Popover>
+
+                        </li>
+                        <li className='flex items-center text-white gap-3'>
                             <img src="/About.png" className='size-7' alt="" /> <p>About Us</p>
                         </li>
                     </ul>
