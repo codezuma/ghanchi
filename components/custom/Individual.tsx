e5c4619a441f5d657f292471eb9152712ad7a18d
siddhant ota <siddhantota@protonmail.com>
2024-02-28 17:37:35 +0530

    fix

--- components/custom/Individual.tsx
+++ components/custom/Individual.tsx
@@ -14,7 +14,7 @@ const Individual = ({
     img: string[]
 }) => {
     return (
-        <div className='containerMod'>
+        <div className=''>
             <p className='bg-[#FB2963] py-2 text-center tracking-wide text-2xl font-semibold text-white'>{heading}</p>
             <div className="my-4 max-w-[80%] mx-auto">
                 <AspectRatio className='border' ratio={16 / 9}>
@@ -23,10 +23,10 @@ const Individual = ({
             </div>
             <p className='px-4'>{para}</p>
             <p className='font-semibold text-white text-center bg-[#002246] py-2 my-6'>SAMPLES</p>
-            <div className='flex justify-between items-center'>
-                <img src={img[0]} alt="" />
-                <img src={img[1]} alt="" />
-                <img src={img[2]} alt="" />
+            <div className='flex w-full overflow-hidden justify-between items-center'>
+                <img className='w-full' src={img[0]} alt="" />
+                <img className='w-full' src={img[1]} alt="" />
+                <img className='w-full' src={img[2]} alt="" />
             </div>
         </div>
     )
