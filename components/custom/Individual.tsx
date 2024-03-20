66053bb63cc9f0023f83dc4c5dc68db81b8088f5
siddhant ota <siddhantota@protonmail.com>
2024-03-20 11:07:29 +0530

    fixes

--- components/custom/Individual.tsx
+++ components/custom/Individual.tsx
@@ -21,7 +21,7 @@ const Individual = ({
 }) => {
     return (
         <div className='border'>
-            <p className='bg-[#FB2963] py-2 text-center tracking-wide text-2xl font-semibold text-white'>{heading}</p> 
+            <p className='bg-[#FB2963] py-2 text-center tracking-wide text-2xl font-semibold text-white'>{heading}</p>
             <p className='px-4 text-center containerMod'>{para}</p>
             <p className='font-semibold text-white text-center bg-[#002246] py-2 my-6'>SAMPLES</p>
             <Carousel className="flex my-10 w-full lg:max-w-[50%] overflow-hidden md:overflow-visible lg:mx-auto justify-evenly items-center">
@@ -32,7 +32,7 @@ const Individual = ({
                         img.map((imageUrl: string, index: number) => {
                             return (
                                 <CarouselItem key={index} className="basis-1/3 md:basis-1/2 lg:basis-1/3">
-                                    <img src={imageUrl} alt="" />
+                                    <img src={imageUrl} className='w-36 h-48' alt="" />
                                 </CarouselItem>
                             )
                         })
